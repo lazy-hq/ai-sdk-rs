6194d6bbd795ffe76f3753e5b1696e1b78238d82
surafel_fikru <surafelfikru76@gmail.com>
2025-09-07 18:53:19 +0300

    refactor(core): create macro for redundunt fields language model options

--- src/core/types.rs
+++ src/core/types.rs
@@ -7,13 +7,72 @@ use std::pin::Pin;
 
 use crate::error::{Error, Result};
 
-/// Options for a `generate_text` call.
-#[derive(Debug, Clone, Serialize, Deserialize, Builder)]
-#[builder(pattern = "owned", setter(into), build_fn(error = "Error"))]
-pub struct GenerateTextCallOptions {
-    /// The prompt to generate text from.
-    pub prompt: String,
+/// Shortens the definition of the `GenerateTextCallOptions` and
+/// `LanguageModelCallOptions` because all the fields from
+/// `LanguageModelCallOptions` are also in `GenerateTextCallOptions`.
+macro_rules! define_with_lm_call_options {
+        ( $( ($field:ident, $typ:ty, $default:expr, $comment:expr) ),* ) => {
+            #[derive(Debug, Clone, Serialize, Deserialize, Builder)]
+            #[builder(pattern = "owned", setter(into), build_fn(error = "Error"))]
+            pub struct GenerateTextCallOptions  {
+                $(
+                    #[doc = $comment]
+                    #[builder(default = $default)]
+                    pub $field: $typ,
+                )*
+                // Define `GenerateTextCallOptions` specific entries here
+
+                /// Maximum number of retries.
+                #[builder(default = "100")]
+                pub max_retries: u32,
+            }
+
+            /// Options for a language model request. The ones directly passed to the
+            /// provider,`None` is used for the provider default.
+            #[derive(Debug, Clone, Serialize, Deserialize, Builder)]
+            #[builder(pattern = "owned", setter(into), build_fn(error = "Error"))]
+            pub struct LanguageModelCallOptions {
+                $(
+                    #[doc = $comment]
+                    #[builder(default = $default)]
+                    pub $field: $typ,
+                )*
+            }
+
+        };
 }
+
+define_with_lm_call_options!(
+    // identifier, type, default, comment
+    (
+        prompt,
+        String,
+        "".to_string(),
+        "The prompt to generate text from. uses the completion format. If both prompt and messages are set, prompt will be ignored."
+    ),
+    (
+        system_prompt,
+        Option<String>,
+        None,
+        "The system prompt to generate text from."
+    ),
+    (
+        messages,
+        Option<Vec<String>>,
+        None,
+        "The messages to generate text from. uses the chat format. If both prompt and messages are set, prompt will be ignored."
+    ),
+    (
+        max_tokens,
+        Option<u32>,
+        None,
+        "The maximum number of tokens to generate."
+    ),
+    (temprature, Option<u32>, None, "Randomness."),
+    (top_p, Option<u32>, None, "Nucleus sampling."),
+    (top_k, Option<u32>, None, "Top-k sampling."),
+    (stop, Option<Vec<String>>, None, "Stop sequence.")
+);
 
 impl GenerateTextCallOptions {
     /// Creates a new builder for `GenerateTextCallOptions`.
@@ -36,25 +95,17 @@ impl GenerateTextResponse {
     }
 }
 
-/// Response from a `generate_stream` call.
-pub struct GenerateStreamResponse {
-    /// A stream of responses from the language model.
-    pub stream: LanguageModelStreamingResponse,
-}
-
-/// Options for a language model request.
-#[derive(Debug, Clone, Serialize, Deserialize, Builder)]
-#[builder(pattern = "owned", setter(into), build_fn(error = "Error"))]
-pub struct LanguageModelCallOptions {
-    /// The prompt to generate text from.
-    pub prompt: String,
-}
-
 impl LanguageModelCallOptions {
     /// Creates a new builder for `LanguageModelCallOptions`.
     pub fn builder() -> LanguageModelCallOptionsBuilder {
         LanguageModelCallOptionsBuilder::default()
     }
+}
+
+/// Response from a `generate_stream` call.
+pub struct GenerateStreamResponse {
+    /// A stream of responses from the language model.
+    pub stream: LanguageModelStreamingResponse,
 }
 
 /// Response from a language model.
