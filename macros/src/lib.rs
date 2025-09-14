bb210e80d6d1635417d1234fe00b5c73d7f5f74e
surafel_fikru <surafelfikru76@gmail.com>
2025-09-14 13:45:30 +0300

    feat(core): implement tool_factory macro
    macro only generates tool with name and description and lacks schema and
    execute attributes
    
    feat(core): implement tool_factory macro

---
+++ macros/src/lib.rs
@@ -0,0 +1,105 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Expr, ExprLit, FnArg, ItemFn, Lit, Meta, Pat, ReturnType};
+
+#[proc_macro_attribute]
+pub fn tool_factory(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input_fn = parse_macro_input!(item as ItemFn);
+    let fn_name = &input_fn.sig.ident;
+    let vis = &input_fn.vis;
+    let block = &input_fn.block;
+    let inputs = &input_fn.sig.inputs;
+    let attrs = &input_fn.attrs;
+
+    // Extract doc comments
+    let doc_comments: Vec<String> = attrs
+        .iter()
+        .filter_map(|attr| {
+            if attr.path().is_ident("doc") {
+                if let Meta::NameValue(meta_name_value) = &attr.meta {
+                    if let Expr::Lit(ExprLit {
+                        lit: Lit::Str(lit_str),
+                        ..
+                    }) = &meta_name_value.value
+                    {
+                        let doc = lit_str.value();
+                        Some(doc)
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let description = doc_comments.join("\n");
+
+    let struct_fields = inputs.iter().filter_map(|arg| {
+        if let FnArg::Typed(pat_type) = arg {
+            if let Pat::Ident(pat_ident) = &*pat_type.pat {
+                let ident = &pat_ident.ident;
+                let ty = &*pat_type.ty;
+                Some(quote! { #ident: #ty })
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    });
+
+    let binding_tokens: Vec<_> = inputs.iter().filter_map(|arg| {
+        if let FnArg::Typed(pat_type) = arg {
+            if let Pat::Ident(pat_ident) = &*pat_type.pat {
+                let ident = &pat_ident.ident;
+                let ty = &*pat_type.ty;
+                let ident_str = ident.to_string();
+                Some(quote! {
+                    let #ident: #ty = serde_json::from_value(
+                        inp.remove(#ident_str)
+                            .unwrap_or_else(|| todo!("Missing required parameter: {}", #ident_str))
+                    ).unwrap_or_else(|e| {
+                        todo!("Failed to deserialize {}: {}", #ident_str, e)
+                    });
+                })
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }).collect();
+
+    let expanded = quote! {
+        #vis fn #fn_name() -> Tool {
+            let mut tool = Tool::new();
+
+            #[derive(JsonSchema, Serialize, Debug)]
+            struct ToolInput {
+                #(#struct_fields),*
+            }
+
+            tool.name = stringify!(#fn_name).to_string();  // TODO: Change to better formatted text
+            // than snake case
+            tool.description = #description.to_string();
+            tool.input_schema = schema_for!(ToolInput);
+            tool.execute = Box::new(|mut inp: HashMap<String, serde_json::Value>| -> Result<String> {
+                // TODO: Do `input_schema` validation on inp
+                // Extract all parameters from the HashMap here
+                #(#binding_tokens)*
+                if !inp.is_empty() {
+                    return Err(Error::Other(format!("Unexpected parameters: {:?}", inp.keys().collect::<Vec<_>>())));
+                }
+                Ok(#block)
+                //Ok(format!("{:?}", result))
+            });
+            tool
+        }
+    };
+
+    TokenStream::from(expanded)
+}
--- src/core/tools.rs
+++ src/core/tools.rs
@@ -1,36 +1,29 @@
-use crate::error::Result;
-use schemars::{JsonSchema, schema_for};
+use crate::error::{Error, Result};
+use aisdk_macros::tool_factory;
+use schemars::{JsonSchema, Schema, schema_for};
 use serde::Serialize;
-use std::any::Any;
+use serde_json::Value;
+use std::collections::HashMap;
 
-pub struct Tool<Inputs>
-where
-    Inputs: JsonSchema + Serialize + IntoIterator,
-{
-    //pub type ToolInput =
+pub struct Tool {
     /// The name of the tool
     pub name: String,
     /// AI friendly description
     pub description: String,
+    /// Tool inputs
+    pub input_schema: Schema,
     /// The output schema of the tool. AI will use this to generate outputs.
-    pub execute: fn(Inputs) -> Result<String>,
+    pub execute: Box<dyn FnMut(HashMap<String, Value>) -> Result<String>>,
 }
 
-impl<Inputs> Tool<Inputs>
-where
-    Inputs: JsonSchema + Serialize + IntoIterator,
-{
-    pub fn new(name: &str, description: &str, execute: fn(Inputs) -> Result<String>) -> Self {
+impl Tool {
+    pub fn new() -> Self {
         Self {
-            name: name.to_string(),
-            description: description.to_string(),
-            execute,
+            name: "".to_string(),
+            description: "".to_string(),
+            input_schema: Schema::default(),
+            execute: Box::new(|_| Ok("".to_string())),
         }
-    }
-
-    /// Generate JSON schema for the input type
-    pub fn argument_schema(&self) -> schemars::Schema {
-        schema_for!(Inputs)
     }
 }
 
@@ -39,18 +32,53 @@ where
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_argument_json_schema() {
-        type ToolInput = Vec<Option<String>>;
-
-        // This works too
-        //let tool = Tool::new("test", "test", |input: ToolInput| Ok(format!("{:?}", input)));
-        let tool = Tool::<ToolInput>::new("test", "test", |input: ToolInput| {
-            Ok(format!("{:?}", input))
-        });
-
-        let schema = tool.argument_schema();
-        eprintln!("{}", schema.clone().to_value());
-        assert_eq!(schema_for!(ToolInput), schema);
+    #[tool_factory]
+    /// This is The Description of an example tool.
+    pub fn my_example_tool(a: u8, b: Option<u8>) -> Tool {
+        format!("{}{}", a, b.unwrap_or(0))
     }
+
+    #[test]
+    fn test_macro() {
+        schemars::schema_for!(String);
+        let mut tool = my_example_tool();
+
+        assert_eq!(tool.name, "my_example_tool");
+        assert_eq!(
+            tool.description,
+            " This is The Description of an example tool."
+        );
+        let schema_properties = tool
+            .input_schema
+            .as_object()
+            .unwrap()
+            .get("properties")
+            .unwrap();
+        assert_eq!(
+            schema_properties.get("a").unwrap().get("format").unwrap(),
+            &serde_json::Value::String("uint8".to_string())
+        );
+        assert_eq!(
+            schema_properties.get("b").unwrap().get("format").unwrap(),
+            &serde_json::Value::String("uint8".to_string())
+        );
+        assert_eq!(
+            schema_properties.get("b").unwrap().get("type").unwrap(),
+            &serde_json::Value::Array(vec![
+                serde_json::Value::String("integer".to_string()),
+                serde_json::Value::String("null".to_string())
+            ])
+        );
+        assert_eq!(
+            (tool.execute)(HashMap::from([
+                ("a".to_string(), 1.into()),
+                ("b".to_string(), Option::<u32>::None.into())
+            ]))
+            .unwrap(),
+            "10".to_string()
+        );
+    }
+
+    #[test]
+    fn test_argument_json_schema() {}
 }
