67a702ac0391ec29f91e26af5448ea1eefd2a5db
ishaksebsib <ishaksebsib@gmail.com>
2025-09-17 16:48:16 +0300

    feat(messages): add type safe builder for easy and safe message list building

--- src/core/types/messages.rs
+++ src/core/types/messages.rs
@@ -16,8 +16,11 @@ pub enum Message {
     Assistant(AssistantMessage),
 }
 
-//TODO: add Message impl for easy creation of messages + type safety that guarantees system
-//prompts are always first
+impl Message {
+    pub fn builder() -> MessageBuilder<Initial> {
+        MessageBuilder::builder()
+    }
+}
 
 /// System model message.
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -63,6 +66,75 @@ impl AssistantMessage {
         Self {
             role: Role::Assistant,
             content: content.into(),
+        }
+    }
+}
+
+/// Message State for type safe message list construction.
+/// Initial state for initial message builder with either system or user message.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Initial;
+
+/// Message State for type safe message list construction.
+/// Conversation state is used for only user and assistant message builder.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Conversation;
+
+/// Message Builder with state for type safe message list construction.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct MessageBuilder<State = Initial> {
+    messages: Vec<Message>,
+    state: std::marker::PhantomData<State>,
+}
+
+impl MessageBuilder {
+    pub fn builder() -> MessageBuilder<Initial> {
+        MessageBuilder {
+            messages: Vec::new(),
+            state: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<State> MessageBuilder<State> {
+    pub fn build(self) -> Vec<Message> {
+        self.messages
+    }
+}
+
+impl MessageBuilder<Initial> {
+    pub fn system(mut self, content: impl Into<String>) -> MessageBuilder<Conversation> {
+        self.messages
+            .push(Message::System(SystemMessage::new(content)));
+        MessageBuilder {
+            messages: self.messages,
+            state: std::marker::PhantomData,
+        }
+    }
+
+    pub fn user(mut self, content: impl Into<String>) -> MessageBuilder<Conversation> {
+        self.messages.push(Message::User(UserMessage::new(content)));
+        MessageBuilder {
+            messages: self.messages,
+            state: std::marker::PhantomData,
+        }
+    }
+}
+
+impl MessageBuilder<Conversation> {
+    pub fn user(mut self, content: impl Into<String>) -> MessageBuilder<Conversation> {
+        self.messages.push(Message::User(UserMessage::new(content)));
+        MessageBuilder {
+            messages: self.messages,
+            state: std::marker::PhantomData,
+        }
+    }
+    pub fn assistant(mut self, content: impl Into<String>) -> MessageBuilder<Conversation> {
+        self.messages
+            .push(Message::Assistant(AssistantMessage::new(content)));
+        MessageBuilder {
+            messages: self.messages,
+            state: std::marker::PhantomData,
         }
     }
 }
