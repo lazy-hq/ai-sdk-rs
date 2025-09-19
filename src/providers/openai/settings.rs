50575353dff88fcf877ad3a6b836a1e1865659ea
ishaksebsib <ishaksebsib@gmail.com>
2025-09-19 18:28:08 +0300

    feat(openai): add base url on openai settings

--- src/providers/openai/settings.rs
+++ src/providers/openai/settings.rs
@@ -9,6 +9,9 @@ use crate::{error::Error, providers::openai::OpenAI};
 /// Settings for the OpenAI provider.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct OpenAIProviderSettings {
+    /// The API base URL for the OpenAI API.
+    pub base_url: String,
+
     /// The API key for the OpenAI API.
     pub api_key: String,
 
@@ -27,6 +30,7 @@ impl OpenAIProviderSettings {
 }
 
 pub struct OpenAIProviderSettingsBuilder {
+    base_url: Option<String>,
     api_key: Option<String>,
     provider_name: Option<String>,
     model_name: Option<String>,
@@ -50,13 +54,17 @@ impl OpenAIProviderSettingsBuilder {
 
     pub fn build(self) -> Result<OpenAI, Error> {
         let settings = OpenAIProviderSettings {
+            base_url: self.base_url.unwrap_or_default(),
             api_key: self.api_key.unwrap_or_default(),
             provider_name: self.provider_name.unwrap_or_else(|| "openai".to_string()),
             model_name: self.model_name.unwrap_or_else(|| "gpt-4o".to_string()),
         };
 
-        let client =
-            Client::with_config(OpenAIConfig::new().with_api_key(settings.api_key.to_string()));
+        let client = Client::with_config(
+            OpenAIConfig::new()
+                .with_api_base(settings.base_url.to_string())
+                .with_api_key(settings.api_key.to_string()),
+        );
 
         Ok(OpenAI { settings, client })
     }
@@ -65,6 +73,7 @@ impl OpenAIProviderSettingsBuilder {
 impl Default for OpenAIProviderSettingsBuilder {
     fn default() -> Self {
         Self {
+            base_url: Some("https://api.openai.com/v1/".to_string()),
             api_key: Some(std::env::var("OPENAI_API_KEY").unwrap_or_default()),
             provider_name: Some("openai".to_string()),
             model_name: Some("gpt-4o".to_string()),
